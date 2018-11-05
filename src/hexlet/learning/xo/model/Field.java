b1e9545fd03565c327c39b68b00fd2ecfe67f3a9
SergeyGolidonov <mailto:3apa3a86@inbox.ru>
2018-11-05 20:38:55 +0300

    Field checks have been added

--- src/hexlet/learning/xo/model/Field.java
+++ src/hexlet/learning/xo/model/Field.java
@@ -1,7 +1,7 @@
 package hexlet.learning.xo.model;
 
 import hexlet.learning.xo.model.exceptions.AlreadyOccupiedException;
-import hexlet.learning.xo.model.exceptions.InvalidePointException;
+import hexlet.learning.xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
@@ -19,17 +19,17 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) throws InvalidePointException {
+    public Figure getFigure(final Point point) throws InvalidPointException {
         if (!checkPoint(point)) {
-            throw new InvalidePointException();
+            throw new InvalidPointException();
         }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) throws InvalidePointException,
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException,
                                                                         AlreadyOccupiedException {
         if (!checkPoint(point)) {
-            throw new InvalidePointException();
+            throw new InvalidPointException();
         }
         if (field[point.x][point.y] != null) {
             throw new AlreadyOccupiedException();
